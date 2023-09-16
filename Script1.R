9fb7da15cd56b268cc332b9108eff7078c22e659
RohanBaghel10 <rbag0005@student.monash.edu>
2023-09-17 05:59:18 +1000

    changes to scalie in IDA and Forecasting on Oxytocin

--- Script1.R
+++ Script1.R
@@ -1,3 +1,23 @@
 library(tidyverse)
 
 data <- read_csv(here::here("data/famillyplanning_maternal_child_health_products.csv"))
+
+unique_regions <- unique(tsibble_data$region)
+unique_items <- unique(tsibble_data$item)
+
+create_autoplot <- function(data, item_name) {
+  data_filtered <- data %>% 
+    filter(item == item_name)
+  
+  autoplot(data_filtered) +
+    facet_wrap(~ region, scales = "free") +
+    theme(legend.position = "none")
+}
+
+create_autoplot(tsibble_data, "Amoxicillin - 125mg - Tablet (Dispersible)")
+
+create_autoplot(tsibble_data, "Amoxicillin - 250mg - Tablet (Dispersible)")
+
+create_autoplot(tsibble_data,"Chlorhexidine digluconate - 7.1% ( which contains 4% Chlorhexidine) - Gel" )
+
+
